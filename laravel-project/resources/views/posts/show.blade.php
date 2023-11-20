4301481693593fffe9d2c1f88852ade2112c1cde
haruka-ochiai <pw.g1.po1122@gmail.com>
2023-11-20 12:48:42 +0900

    finish crud

---
+++ laravel-project/resources/views/posts/show.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header">記事詳細</div>
+
+        <div class="card-body">
+          <div class="table-resopnsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>タイトル</th>
+                  <th>本文</th>
+                </tr>
+              </thead>
+              <tbody>
+                @if(isset($post))
+                <tr>
+                  <td>{{ $post->title }}</a></td>
+                  <td>{{ $post->body }}</td>
+                </tr>
+                @endif
+              </tbody>
+            </table>
+            @if(isset($post))
+            <div class="text-center">
+                <button type="button" class="btn btn-secondary" onClick="history.back()">戻る</button>
+                <form style="display:inline" action="{{ route('posts.destroy', $post->id) }}" method="post">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-danger ml-3">
+                        {{ __('削除') }}
+                    </button>
+                </form>
+                <button type="button" class="btn btn-primary ml-3" onClick="location.href='{{ route('posts.edit', $post->id) }}'">
+                    編集
+                </button>
+            </div>
+            @endif
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
